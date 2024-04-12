8a0003fbf8412d3634f91bbec511b2d4a63b256e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-04-12 11:03:27 -0300

    Criar projeto

---
+++ src/main/java/com/vinimanfrin/inscreening/InscreeningApplication.java
@@ -0,0 +1,13 @@
+package com.vinimanfrin.inscreening;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InscreeningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InscreeningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinimanfrin/inscreening/InscreeningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinimanfrin.inscreening;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InscreeningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
